1d2d6e3c75bec17f24b11610d20f8b2d73c0ce8c
Tehan Uys <nwu24297704@gmail.com>
2020-10-04 17:44:01 +0200

    Added Footer(V1). Added Header(V1). Added Content for Home Page(WIP). Added Image. Some Basic Styling(WIP). Added Splash Screen/Loader.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from '@angular/core'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HomeComponent } from './home/home.component'
+import { FooterComponent, HeaderComponent } from './shared/components'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
